42b5c52a55b33cd824bb36ebbaf09a0e8cf2b645
R1t1sh <ritishrratan5@gmail.com>
2024-07-26 19:00:58 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Logo = () => {
+                  const logoUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEN591o61lwI1joEDO_pG8ireC95w6qf52qg&s';
+   return <img src={logoUrl} alt="Logo" />;
+};
+export default Logo
